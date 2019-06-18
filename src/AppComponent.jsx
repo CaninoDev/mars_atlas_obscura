3563946352473eff178f949fb9700b8616a24319
Canino Developments <ian@thecommittedbug.io>
2019-06-18 18:56:04 -0400

    separation of concerns for App

---
+++ src/AppComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AppComponent = props => (
+    <div>
+        This is AppComponent!
+    </div>
+);
+
+export default AppComponent;
---
+++ src/AppContainer.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import AppComponent from './AppComponent';
+
+class AppContainer extends Component {
+
+  render () {
+    return (
+      <React.Fragment>
+        <AppComponent />
+      </React.Fragment>
+    )
+  }
+}
+export default AppContainer;
