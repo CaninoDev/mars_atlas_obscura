eb358e0785863d3b7e72bb2656ebccc1c09869f8
Ian G Canino <ian.canino@gmail.com>
2019-06-22 14:23:07 -0400

    implmeent basic styled form

--- src/AppComponent.jsx
+++ src/AppComponent.jsx
@@ -1,8 +1,35 @@
 import React from 'react';
 
-const AppComponent = props => (
+const AppComponent = ({onChange, onSubmit}) => (
     <div>
-        This is AppComponent!
+        <h1>Mars Photo API</h1>
+        <br/>
+        <div>
+            <h2>Curiosity</h2>
+            <form onSubmit={onSubmit}>
+                <label>
+                    Sol<br/>
+                    <input id="sol=date" type="number" name="sol-date" />
+                </label>
+                <br/>
+                <br />
+                <label>
+                    Camera<br/>
+                    <select name="cameras">
+                        <option value="fhaz" selected>Front Hazard</option>
+                        <option value="rhaz">Front Hazard Avoidance</option>
+                        <option value="mast">Mast</option>
+                        <option value="chemcam">Chemistry and Camera Comnplex</option>
+                        <option value="mahli">Mars Hand Lens Imager</option>
+                        <option value="mardi">Mars Descent Imager</option>
+                        <option value="navcam">Navigation</option>
+                    </select>
+                </label>
+                <br />
+                <br />
+                <button type="submit">Find Photos</button>
+            </form>
+        </div>
     </div>
 );
 
