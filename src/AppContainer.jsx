ec379d644a8368e4ce797eddb43b28e6684b4f19
Ian G Canino <ian.canino@gmail.com>
2019-06-22 14:24:02 -0400

    template form state and function

--- src/AppContainer.jsx
+++ src/AppContainer.jsx
@@ -2,11 +2,29 @@ import React, { Component } from 'react';
 import AppComponent from './AppComponent';
 
 class AppContainer extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      solDate: 0,
+      camera: null
+    }
+    this.onSubmit = this.onSubmit.bind(this)
+    this.onChange = this.onChange.bind(this)
+  }
+
+  onSubmit = e => {
+
+  }
+
+  onChange = e => {
+
+  }
 
   render () {
     return (
       <React.Fragment>
-        <AppComponent />
+        <AppComponent onSubmit={this.onSubmit} onChange={this.onChange}/>
       </React.Fragment>
     )
   }
