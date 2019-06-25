c30420047aea35d21a27ac31407f2c0c30f572db
Canino Developments <ian@thecommittedbug.io>
2019-06-25 14:20:14 -0400

    add another endpoint to request validation data

--- server.go
+++ server.go
@@ -52,11 +52,44 @@ func queryCameraPhotos(p *url.Values) ([]byte, error) {
 
 }
 
+func handleManifestsQuery(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
+
+	response, err := queryManifests()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(response)
+}
+
+func queryManifests() ([]byte, error) {
+	u := url.URL{
+		Scheme: "http",
+		Host: "mars-photos.herokuapp.com",
+		Path: "/api/v1/manifests/Curiosity",
+	}
+
+	response, err := httpClient.Get(u.String())
+	if err != nil {
+		panic(err.Error())
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return []byte(body), nil
+}
+
 func enableCors(w *http.ResponseWriter) {
     (*w).Header().Set("Access-Control-Allow-Origin", "*")
 }
 
 func main() {
+	http.HandleFunc("/api/v1/manifests", handleManifestsQuery)
 	http.HandleFunc("/api/v1/", handlePhotosQuery)
 	if err := http.ListenAndServe(":3001", nil); err != nil {
 		panic(err)
