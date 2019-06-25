2f8ad4b2e0d5eb76c83490853e2f1ceb18afe6ef
Canino Developments <ian@thecommittedbug.io>
2019-06-25 11:02:53 -0400

    finalize backend and update version

--- server.go
+++ server.go
@@ -1,17 +1,20 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
+	"net/url"
+	"fmt"
+	"time"
+	"io/ioutil"
 )
+
+var httpClient = &http.Client{Timeout: 10 * time.Second}
 
 func handleQuery(w http.ResponseWriter, r *http.Request) {
 	enableCors(&w)
-
 	// read the query from URL params
 	params := r.URL.Query()
-
-	response, err := json.Marshal(params)
+	response, err := queryNASA(&params)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -20,8 +23,37 @@ func handleQuery(w http.ResponseWriter, r *http.Request) {
 	w.Write(response)
 }
 
+func queryNASA(p *url.Values) ([]byte, error) {
+	u := url.URL{
+		Scheme: "http",
+		Host: "mars-photos.herokuapp.com",
+		Path: "/api/v1/rovers/Curiosity/photos",
+	}
+
+	q := u.Query()
+	q.Set("camera", p.Get("camera"))
+	q.Set("sol", p.Get("sol"))
+	u.RawQuery = q.Encode()
+
+	fmt.Println(u.String())
+
+	response, err := httpClient.Get(u.String())
+	if err != nil {
+		panic(err.Error())
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return []byte(body), nil
+
+}
+
 func enableCors(w *http.ResponseWriter) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+    (*w).Header().Set("Access-Control-Allow-Origin", "*")
 }
 
 func main() {
